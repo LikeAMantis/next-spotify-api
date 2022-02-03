0d82a571614539009b731b6381273aec83b57018
David <d.guetl90@gmail.com>
2022-02-03 13:29:04 +0100

    middleware log

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,6 +4,10 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
     console.log("token", token);
+    console.log("secret", process.env.JWT_SECRET);
+    console.log("req", req);
+    console.log("NEXTAUTH_URL", process.env.NEXTAUTH_URL);
+
     const { pathname } = req.nextUrl
 
 
