fd140a3d3200e022d9a1256f8f31cad752f4a9a2
David <d.guetl90@gmail.com>
2022-02-04 11:42:29 +0100

    NEXTAUTH_SECRET

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,14 +2,12 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
-    // console.log("token", token);
-    // console.log("secret", process.env.JWT_SECRET);
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const { pathname } = req.nextUrl
+    console.log("token", token);
+    // console.log("secret", process.env.NEXTAUTH_SECRET);
     // console.log("req", req);
     // console.log("NEXTAUTH_URL", process.env.NEXTAUTH_URL);
-
-    const { pathname } = req.nextUrl
-
 
     if (pathname.includes("/api/auth") || token) {
         console.log("middleware next");
